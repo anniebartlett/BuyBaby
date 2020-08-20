cfa006d45a7b7b268fab352c6085b84d5f9cae72
Annie Bartlett <annie-bartlett@outlook.com>
2020-08-20 20:17:27 +0100

    add routes and product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
-class PagesController < ApplicationController
+class ProductsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+
   end
 end
