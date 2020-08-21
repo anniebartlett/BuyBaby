6f266dfd329eacb5c0a2119fd54d5b3eb0770676
nouria chaouy <nouria.chaouy@gmail.com>
2020-08-21 09:06:01 +0100

    added controllers, actions and views

--- db/migrate/20200818183548_create_orders.rb
+++ db/migrate/20200818183548_create_orders.rb
@@ -1,7 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-      t.boolean :completed
+      t.boolean :completed, default: false
       t.integer :price_cent
       t.references :user
 
