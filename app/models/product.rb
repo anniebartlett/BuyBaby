6af5189d53774be70394ea787561de96610f19de
Annie Bartlett <annie-bartlett@outlook.com>
2020-08-22 17:40:21 +0100

    set up seed scraper

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,18 +4,18 @@ class Product < ApplicationRecord
   has_many :product_order
   has_many :orders, through: :product_order
   has_many_attached :photos
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+  # geocoded_by :address
+  # after_validation :geocode, if: :will_save_change_to_address?
 
   validates :name, presence: true
   validates :description, presence: true
   validates :location, presence: true
-  validates :longitude, presence: true
-  validates :latitude, presence: true
+  # validates :longitude, presence: true
+  # validates :latitude, presence: true
   validates :condition, presence: true
   validates :size, presence: true
-  validates :payment_option, inclusion: { in: ["Card Payment", "Cash Payment"]}
+  validates :payment_options, inclusion: { in: ["Card Payment", "Cash Payment"]}
   validates :stripe_plan_name, presence: true
-  monetize :price_cents
-  validates :delivery_option, inclusion: { in: ["Collect from chosen location", "Deliver Home", "Arrange pick-up"]}
+  # monetize :price_cents
+  validates :deliver_option, inclusion: { in: ["Collect from chosen location", "Deliver Home", "Arrange pick-up"]}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,62 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+
+prams = "https://www.mamasandpapas.com/en-gb/c/travel/pushchairs/pushchairs-all/pushchairs-prams"
+crib = "https://www.mamasandpapas.com/en-gb/c/nursery/nursery-furniture/cots-cribs-cotbeds"
+toys = "https://www.mamasandpapas.com/en-gb/c/toys-gifts/playtime"
+boys_clothing = "https://www.mamasandpapas.com/en-gb/c/clothing/boys-clothing"
+girls_clothing = "https://www.mamasandpapas.com/en-gb/c/clothing/girls-clothing"
+
+def clean_database
+  puts "Cleaning database..."
+  Product.destroy_all
+  User.destroy_all
+  Order.destroy_all
+  puts "Database clean..."
+end
+
+def create_user
+  puts "Creating user..."
+  user = User.create(name: 'lisa', email: 'lisa@me.com', password: '123456', nickname: 'lisa_sells_stuff', description: 'Good seller')
+  puts "Created #{user.name}"
+end
+
+def scrape_product(product)
+  puts "Creating products..."
+
+  category = product.split("/")[5].capitalize
+
+  doc = Nokogiri::HTML(open("#{product}"))
+
+    doc.search('.productCard').each do |element|
+      title = element.search('.productCard_title').text.strip
+      price = element.search('.productCard_price').text.strip
+      # img = element.search('img').attribute('data-src').value
+
+        products = Product.create(
+          user_id: User.first.id,
+          name: title,
+          description: "In great condition, your kids will love this!",
+          location: "London",
+          condition: %w[Used Good New].sample,
+          size: %w[Small Medium Large].sample,
+          colour: "As per product",
+          payment_options: ["Card Payment", "Cash Payment"].sample,
+          category: category,
+          stripe_plan_name: "Test",
+          price_cent: price,
+          deliver_option: ["Collect from chosen location", "Deliver home", "Arrange pick-up"].sample
+        )
+        puts "Created #{products.id} - #{products.name}"
+    end
+  # product.photos.attach(io: file, filename: 'plant.png', content_type: 'image/png')
+end
+
+clean_database
+create_user
+scrape_product(prams)
+scrape_product(crib)
+scrape_product(toys)
+scrape_product(boys_clothing)
+scrape_product(girls_clothing)
+
+puts "Finished!"
