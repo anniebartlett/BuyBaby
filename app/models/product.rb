f5d3c3077b9e75215f93ec90b140f10b5458afcf
Annie Bartlett <annie-bartlett@outlook.com>
2020-08-25 19:45:54 +0100

    add cloudinary and updated seed file to include imgs

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,22 +1,25 @@
 class Product < ApplicationRecord
   belongs_to :user
 
+  DELIVERY_OPTIONS = ["Collect from chosen location", "Deliver Home", "Arrange pick-up"]
+  PAYMENT_OPTIONS = ["Card Payment", "Cash Payment"]
+
   has_many :product_orders
   has_many :orders, through: :product_orders
   has_many_attached :photos
   # geocoded_by :address
   # after_validation :geocode, if: :will_save_change_to_address?
 
-  validates :name, presence: true
+  # validates :name, presence: true
   validates :description, presence: true
   validates :location, presence: true
   # validates :longitude, presence: true
   # validates :latitude, presence: true
   validates :condition, presence: true
   validates :size, presence: true
-  validates :payment_options, inclusion: { in: ["Card Payment", "Cash Payment"]}
+  validates :payment_options, inclusion: { in: PAYMENT_OPTIONS }
   # validates :stripe_plan_name, presence: true
   # monetize :price_cents
-  validates :deliver_option, inclusion: { in: ["Collect from chosen location", "Deliver Home", "Arrange pick-up"]}
 
+  validates :deliver_option, inclusion: { in: DELIVERY_OPTIONS }
 end
