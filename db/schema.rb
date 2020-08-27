1f3971983f5964c91c362a54495f7badab078acf
martin lowe <martinlowe673@gmail.com>
2020-08-27 21:21:28 +0100

    added geocode and added to product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_175119) do
+ActiveRecord::Schema.define(version: 2020_08_27_200118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,8 @@ ActiveRecord::Schema.define(version: 2020_08_25_175119) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "sku"
     t.integer "price_cents", default: 0, null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
