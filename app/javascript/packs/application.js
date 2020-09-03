fba8112397d795e55c24b366f90ba430546c5cb5
Annie Bartlett <annie-bartlett@outlook.com>
2020-09-03 23:04:58 +0100

    update index page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,12 +24,14 @@ require("channels");
 import "bootstrap";
 import { initStarRating } from "../plugins/init_star_rating";
 import { initSweetalert } from "../plugins/init_sweetalert";
+import flatpickr from "flatpickr";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener("turbolinks:load", () => {
   initStarRating();
+  flatpickr(".datepicker", {});
   initSweetalert("#sweet-alert-demo", {
     title: "A nice alert",
     text: "This is a great alert, isn't it?",
