32e49d43dcc709c72f19854d5cb4c576982ffc82
Ayan Ali <ayanmohamud.ali@hotmail.com>
2020-09-10 16:43:39 +0100

    updated homepage

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,9 @@ require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
-//= require jquery.lettering-0.6.1.min
 //= require jquery
 //= require jquery_ujs
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -48,5 +48,6 @@ document.addEventListener("turbolinks:load", () => {
   expandableSearchBar();
   // Call your functions here, e.g:
   // initSelect2();
-  $(".carousel").carousel();
+
+  // $(".carousel").carousel();
 });
