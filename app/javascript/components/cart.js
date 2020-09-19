8c27267cb21fee9875dc0ba4a4aa1b5e48698acd
nouria chaouy <nouria.chaouy@gmail.com>
2020-09-19 14:12:22 +0100

    added order total method and modified order views

--- app/javascript/components/cart.js
+++ app/javascript/components/cart.js
@@ -1,9 +1,22 @@
+
+// const minusButtons = document.querySelectorAll('.minus-btn')
+// minusButtons.forEach((button) => {
+//   button.addEventListener('click', (event) => {
+//     console.log(button.dataset.productprice);
+//     let Bbvalue = parseInt(document.getElementById('btn-value').value);
+//     Btval = Bbvalue -= 1
+//     Btval.innerText = `Total: £${Btval}`
+//   })
+// })
+
+
 $('.minus-btn').on('click', function(e) {
     e.preventDefault();
     let $this = $(this);
     let $input = $this.closest('div').find('input');
     let value = parseInt($input.val());
 
+//console.log($(this).attr('data-product-price'))
     if (value > 1) {
         value -= 1;
         const totalPrice = document.getElementById('price-total');
@@ -23,12 +36,17 @@ $('.plus-btn').on('click', function(e) {
     let $this = $(this);
     let $input = $this.closest('div').find('input');
     let value = parseInt($input.val());
-
+console.log("Good bye!")
     if (value >= 1) {
         value += 1;
         const totalPrice = document.getElementById('price-total');
         const unitPrice = parseInt(document.getElementById('price-item').innerText.split("£")[1])
+        const cartCount = parseInt(document.getElementById('cartCount').innerText);
         const total = value * unitPrice
+
+
+        // unitPrice += value
+        // if value > 1 then cartCount += 1
         totalPrice.innerText = `Total: £${total}`
     } else {
         value = 1;
