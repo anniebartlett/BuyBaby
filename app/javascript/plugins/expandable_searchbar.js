63939c702e6bd8fa714fc3a4bcc37f4c7624177c
Ayan Ali <ayanmohamud.ali@hotmail.com>
2020-09-23 08:07:12 +0100

    expandable searchbar in navbar final

--- app/javascript/plugins/expandable_searchbar.js
+++ app/javascript/plugins/expandable_searchbar.js
@@ -1,11 +1,12 @@
 const expandableSearchBar = () => {
 
-  const searchBtn = document.querySelector(".search-btn");
+  const searchBtn = document.querySelector(".fa-search");
   const searchBox = document.querySelector(".input");
 
   searchBtn.addEventListener('click', () => {
-    searchBox.style.width = '80%';
+    searchBox.style.width = '100%';
     searchBox.style.paddingLeft = '40px';
+    searchBox.style.cursor = 'text';
     searchBox.focus();
     searchBox.setAttribute('placeholder', 'Search for items');
   });
